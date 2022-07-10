e5fd51caf473e77f9ae09df0c4e6bb86458c04ce
VighneshBilgi <vhbilgi@gmail.com>
2022-07-10 16:55:40 +0530

    Added: Temperature Conversion

---
+++ src/test/java/com/bridgelabz/junittesting/TemperatureConversionTest.java
@@ -0,0 +1,51 @@
+package com.bridgelabz.junittesting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class TemperatureConversionTest {
+
+    @Test
+    public void testTemperatureConversion_100Celsius() {
+        double actual = TemperatureConversion.temperatureConversion(100.0,1);
+        assertEquals(212.0,actual,0);
+    }
+
+    @Test
+    public void testTemperatureConversion_0Celsius() {
+        double actual = TemperatureConversion.temperatureConversion(0.0,1);
+        assertEquals(32.0,actual,0);
+    }
+
+    @Test
+    public void testTemperatureConversion_212Fahrenheit() {
+        double actual = TemperatureConversion.temperatureConversion(212,2);
+        assertEquals(100,actual,0);
+    }
+
+    @Test
+    public void testTemperatureConversion_32Fahrenheit() {
+        double actual = TemperatureConversion.temperatureConversion(32,2);
+        assertEquals(0,actual,0);
+    }
+
+    @Test
+    public void testTemperatureConversion_50Celsius() {
+        double actual = TemperatureConversion.temperatureConversion(50,1);
+        assertEquals(122,actual,0);
+    }
+
+    @Test
+    public void testTemperatureConversion_0Fahrenheit() {
+        double actual = TemperatureConversion.temperatureConversion(0,2);
+        assertEquals(-18.0,actual,0);
+    }
+
+    @Test
+    public void testTemperatureConversion_32Celsius() {
+        double actual = TemperatureConversion.temperatureConversion(32,1);
+        assertEquals(90,actual,0);
+    }
+}
