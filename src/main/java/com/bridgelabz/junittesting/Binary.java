1766a9fff2c01919819f0e0133b3c1cc456570a5
VighneshBilgi <vhbilgi@gmail.com>
2022-07-11 01:01:43 +0530

    Added: toBinary

--- src/main/java/com/bridgelabz/junittesting/Binary.java
+++ src/main/java/com/bridgelabz/junittesting/Binary.java
@@ -1,2 +1,70 @@
-package com.bridgelabz.junittesting;public class Binary {
+package com.bridgelabz.junittesting;
+
+import java.util.Scanner;
+
+public class Binary {
+
+    static int power = 1;
+    static int maxPower = 0;
+    static int binaryDigit;
+    static int rem;
+
+    public static String toBinary(int num) {
+        while(num>power){
+
+            power = 2* power;
+
+            if(power>num)
+                break;
+
+            maxPower++;
+
+        }
+
+
+        int[] powersOf2 = new int[maxPower+1];
+
+        for(int i=0; i<(maxPower+1); i++){
+
+            powersOf2[i] = (int)Math.pow(2,i);
+
+        }
+
+        String binary = "";
+
+
+        for (int i = powersOf2.length-1; i >= 0; i--) {
+
+            binaryDigit = num / powersOf2[i];
+            binary = Integer.toString(binaryDigit) + binary;
+
+            rem = num%powersOf2[i];
+            num = rem;
+
+        }
+
+        String binaryString = "";
+        char ch;
+        for (int i=0; i<binary.length(); i++)
+        {
+            ch= binary.charAt(i);
+            binaryString= ch+binaryString;
+        }
+        return binaryString;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter decimal number:");
+        int num = sc.nextInt();
+
+        String binary= toBinary(num);
+
+        System.out.println("The binary equivalent of "+num+" is :"+binary);
+
+    }
+
+
 }
--- src/test/java/com/bridgelabz/junittesting/BinaryTest.java
+++ src/test/java/com/bridgelabz/junittesting/BinaryTest.java
@@ -1,4 +1,27 @@
+package com.bridgelabz.junittesting;
+
 import junit.framework.TestCase;
-public class BinaryTest extends TestCase {
-  
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BinaryTest {
+
+    @Test
+    public void testToBinary106() {
+        String actual = Binary.toBinary(106);
+        assertEquals("1101010",actual);
+    }
+
+    @Test
+    public void testToBinary10() {
+        String actual = Binary.toBinary(10);
+        assertEquals("1010",actual);
+    }
+
+    @Test
+    public void testToBinary38() {
+        String actual = Binary.toBinary(38);
+        assertEquals("100110",actual);
+    }
 }
