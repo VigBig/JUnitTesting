a5aa1d47d724c79326b6bdd8b11f8c7385769c07
VighneshBilgi <vhbilgi@gmail.com>
2022-07-11 02:53:36 +0530

    Added: Swapped Nibbles

--- src/main/java/com/bridgelabz/junittesting/Binary.java
+++ src/main/java/com/bridgelabz/junittesting/Binary.java
@@ -53,6 +53,75 @@ public class Binary {
         return binaryString;
     }
 
+    public static String swapNibbles(String binary) {
+        int nibbleArraySize = binary.length()/4;
+        String[] nibbles = new String[nibbleArraySize];
+
+
+        int k=0;
+        for(int m=0; m<nibbleArraySize;m++){
+            nibbles[m]= binary.substring(k,k+4);
+            k=k+4;
+        }
+
+        System.out.println("Binary number separated into nibbles:");
+        for(int n=0; n<nibbles.length;n++){
+            System.out.print(nibbles[n]+" ");
+        }
+
+        System.out.println();
+        String temp = "";
+        for(int n=0; n<nibbles.length;n++){
+            temp = nibbles[n];
+
+            if(n==nibbles.length-1){
+                break;
+            }
+
+            nibbles[n]=nibbles[n+1];
+            nibbles[n+1]=temp;
+
+
+        }
+
+        System.out.println("Swapped nibbles: ");
+        String swappedNibbles = "";
+        for(int n=0; n<nibbles.length;n++){
+            System.out.print(nibbles[n]+" ");
+            swappedNibbles = swappedNibbles + nibbles[n];
+        }
+        System.out.println();
+
+        return swappedNibbles;
+
+    }
+
+    public static int toDecimal(int swappedNibbles) {
+
+        int decimalArraySize = Integer.toString(swappedNibbles).length();
+
+        int decimalArray[] = new int[decimalArraySize];
+
+        int j=0;
+        while(swappedNibbles!=0)
+        {
+            decimalArray[decimalArraySize-j-1] = swappedNibbles%10;
+            swappedNibbles=swappedNibbles/10;
+            j++;
+        }
+
+        int decimal=0;
+        int n=decimalArraySize-1;
+        for(int m=0; m<decimalArray.length;m++){
+            decimal= decimal + (int)(Math.pow(2,m)) * decimalArray[n];
+            n--;
+        }
+
+        return decimal;
+
+    }
+
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -63,8 +132,45 @@ public class Binary {
         String binary= toBinary(num);
 
         System.out.println("The binary equivalent of "+num+" is :"+binary);
+//        System.out.println("Length of binary string "+binary+" is :"+binary.length());
+
+        int multipleOf4 = 1;
+        int i=1;
+        int padding=0;
+        int swappedNibbles;
+
+        if(binary.length()%4 != 0){
+
+            while(true){
+
+                multipleOf4 = 4 * i;
+
+                if(multipleOf4>binary.length()) {
+                    padding = multipleOf4 - binary.length();
+                    break;
+                }
+
+                ++i;
+
+            }
+
+            for(int j=1; j<=padding;j++){
+               binary = "0"+ binary;
+            }
+
+//            System.out.println("The padded binary equivalent of "+num+" is :"+binary);
+//            System.out.println("Length of padded binary string "+binary+" is :"+binary.length());
+
+            swappedNibbles = Integer.parseInt(swapNibbles(binary));
+
+        }else{
+
+            swappedNibbles = Integer.parseInt(swapNibbles(binary));
+
+        }
+
+        System.out.println("Converting the swapped nibbles "+swappedNibbles+" into decimal equivalent: "+toDecimal(swappedNibbles));
 
     }
-
 
 }
--- src/test/java/com/bridgelabz/junittesting/BinaryTest.java
+++ src/test/java/com/bridgelabz/junittesting/BinaryTest.java
@@ -24,4 +24,40 @@ public class BinaryTest {
         String actual = Binary.toBinary(38);
         assertEquals("100110",actual);
     }
+
+    @Test
+    public void testSwapNibbles01101010() {
+        String actual = Binary.swapNibbles("01101010");
+        assertEquals("10100110",actual);
+    }
+
+    @Test
+    public void testSwapNibbles01100100() {
+        String actual = Binary.swapNibbles("01100100");
+        assertEquals("01000110",actual);
+    }
+
+    @Test
+    public void testSwapNibbles01000110() {
+        String actual = Binary.swapNibbles("01000110");
+        assertEquals("01100100",actual);
+    }
+
+    @Test
+    public void testToDecimal1000110() {
+        int actual = Binary.toDecimal(1000110);
+        assertEquals(70,actual);
+    }
+
+    @Test
+    public void testToDecimal10100110() {
+        int actual = Binary.toDecimal(10100110);
+        assertEquals(166,actual);
+    }
+
+    @Test
+    public void testToDecimal1100010() {
+        int actual = Binary.toDecimal(1100010);
+        assertEquals(98,actual);
+    }
 }
